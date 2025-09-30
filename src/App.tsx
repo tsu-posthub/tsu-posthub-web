5348be68ed0096903ffa31f2b1ac72aa7719b9a9
Nikita Lugachev <nikitalugachev149th@gmail.com>
2025-10-01 05:30:33 +0700

    Add Navbar component and update branding assets

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import RegisterPage from "./pages/Auth/RegisterPage";
 import FeedPage from "./pages/Feed/FeedPage";
 import PostPage from "./pages/Post/PostPage";
 import ProfilePage from "./pages/Profile/ProfilePage";
+import Navbar from "./components/Navbar.tsx";
 
 function App() {
     return (
         <AuthProvider>
             <BrowserRouter>
+                <Navbar />
                 <Routes>
                     <Route path="/login" element={<LoginPage />} />
                     <Route path="/register" element={<RegisterPage />} />
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,71 @@
+﻿import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../hooks/useAuth";
+import { Squeeze as Hamburger } from "hamburger-react";
+import { useState } from "react";
+import posthubLogo from "../assets/posthub_logo.svg";
+import "./Navbar.css";
+
+export default function Navbar() {
+    const { accessToken, logout } = useAuth();
+    const navigate = useNavigate();
+    const [isOpen, setOpen] = useState(false);
+
+    const handleLogout = async () => {
+        logout();
+        navigate("/login");
+        setOpen(false);
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-container">
+                <div className="hamburger">
+                    <Hamburger toggled={isOpen} toggle={setOpen} size={24} color="#fff" />
+                </div>  
+
+                <Link to="/" className="navbar-logo" onClick={() => setOpen(false)}>
+                    <img src={posthubLogo} alt="PostHub Logo" className="navbar-logo-img" />
+                    <span className="navbar-logo-text">TSU PostHub</span>
+                </Link>
+
+                <div className={`navbar-login`}>
+                    {!accessToken ? (
+                        <>
+                            <Link to="/login" className="login-link">
+                                Войти
+                            </Link>
+                        </>
+                    ) : (
+                        <button onClick={handleLogout} className="logout-btn">
+                            Выйти
+                        </button>
+                    )}
+                </div>
+
+                <div className={`navbar-links ${isOpen ? "open" : ""}`}>
+                    <Link to="/" onClick={() => setOpen(false)}>Главная</Link>
+                    {accessToken && (
+                        <Link to="/create" onClick={() => setOpen(false)}>Создать пост</Link>
+                    )}
+                </div>
+
+                <div className={`navbar-auth ${!accessToken && isOpen ? "open" : ""}`}>
+                    {!accessToken ? (
+                        <>
+                            <Link to="/login" className="login-link" onClick={() => setOpen(false)}>
+                                Войти
+                            </Link>
+                            <Link to="/register" className="register-btn" onClick={() => setOpen(false)}>
+                                Регистрация
+                            </Link>
+                        </>
+                    ) : (
+                        <button onClick={handleLogout} className="logout-btn">
+                            Выйти
+                        </button>
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
+}
